5aac91b436f238ca729d90b9cd14b8f003f166fa
evafi <evafilgueirasaxo@gmail.com>
2022-08-01 18:22:24 +0200

    all done

--- src/EjercicioCoche.java
+++ src/EjercicioCoche.java
@@ -1,2 +1,15 @@
 public class EjercicioCoche {
+    public static void main (String[] args){
+        Coche miCoche = new Coche ();
+        miCoche.sumarPuerta();
+        miCoche.sumarPuerta();
+        System.out.println(miCoche.puertas);
+    }
+
+}
+class Coche {
+    public int puertas = 0;
+    public void sumarPuerta () {
+        this.puertas++;
+    }
 }
