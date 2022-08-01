214e2cb6e123f43273a6e13ec168afa3ffc1ff63
evafi <evafilgueirasaxo@gmail.com>
2022-08-01 17:37:26 +0200

    EjercicioSuma done

---
+++ src/EjercicioSuma.java
@@ -0,0 +1,12 @@
+public class EjercicioSuma {
+    public static void main(String[] args) {
+
+        int resultado;
+        resultado = suma(1, 2, 5);
+        System.out.print(resultado);
+    }
+    public static int suma (int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
